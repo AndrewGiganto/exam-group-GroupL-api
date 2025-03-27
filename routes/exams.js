d8c38433e83e1e0a40f3b35ce9db909ffd50ae68
unknown <HH440@ccs.local>
2025-03-27 20:56:04 +0800

    get/push-feature

--- routes/exams.js
+++ routes/exams.js
@@ -6,6 +6,19 @@ let exams = [
     { id: 2, name: "Final Exam" }
 ];
 
-
+// GET /exams - Returns all exams
+router.get('/', (req, res) => {
+    res.json(exams);
+    });
+    
+    // POST /exams - Adds a new exam
+    router.post('/', (req, res) => {
+    const newExam = {
+    id: exams.length + 1,
+    name: req.body.name
+    };
+    exams.push(newExam);
+    res.status(201).json(newExam);
+    });
 
 module.exports = router;
